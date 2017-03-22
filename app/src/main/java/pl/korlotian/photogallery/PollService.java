65df28e4fa29165522691795da552c671432e470
Maria Korlotian <maria.stoklosa@netguru.pl>
2017-03-22 18:29:54 +0100

    Check for new results

--- app/src/main/java/pl/korlotian/photogallery/PollService.java
+++ app/src/main/java/pl/korlotian/photogallery/PollService.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.util.Log;
 
+import java.util.List;
+
 public class PollService extends IntentService {
     private static final String TAG = "PollService";
 
@@ -23,7 +25,28 @@ public class PollService extends IntentService {
             return;
         }
 
-        Log.i(TAG, "Received an intent: " + intent);
+        String query = QueryPreferences.getStoredQuery(this);
+        String lastResultId = QueryPreferences.getLastResultId(this);
+        List<GalleryItem> items;
+
+        if (query == null) {
+            items = new FlickrFetchr().fetchRecentPhotos();
+        } else {
+            items = new FlickrFetchr().searchPhotos(query);
+        }
+
+        if (items.size() == 0) {
+            return;
+        }
+
+        String resultId = items.get(0).getId();
+        if (resultId.equals(lastResultId)) {
+            Log.i(TAG, "Got an old result: " + resultId);
+        } else {
+            Log.i(TAG, "Got a new result: " + resultId);
+        }
+
+        QueryPreferences.setLastResultId(this, resultId);
     }
 
     private boolean isNetworkAvailableAndConnected() {

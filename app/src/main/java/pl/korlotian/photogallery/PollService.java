d91ae03d112dc7013364ffb03f20b544b9f71236
Maria Korlotian <maria.stoklosa@netguru.pl>
2017-04-01 17:24:07 +0200

    Send a broadcast intent

--- app/src/main/java/pl/korlotian/photogallery/PollService.java
+++ app/src/main/java/pl/korlotian/photogallery/PollService.java
@@ -20,6 +20,9 @@ public class PollService extends IntentService {
 
     private static final long POLL_INTERVAL = AlarmManager.INTERVAL_FIFTEEN_MINUTES;
 
+    public static final String ACTION_SHOW_NOTIFICATION =
+            "pl.korlotian.android.photogallery.SHOW_NOTIFICATION";
+
     public static Intent newIntent(Context context) {
         return new Intent(context, PollService.class);
     }
@@ -94,6 +97,8 @@ public class PollService extends IntentService {
 
             NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
             notificationManager.notify(0, notification);
+
+            sendBroadcast(new Intent(ACTION_SHOW_NOTIFICATION));
         }
 
         QueryPreferences.setLastResultId(this, resultId);

4aef14ad2a2646f61772ade17e221eae8ad94af6
Maria Korlotian <maria.stoklosa@netguru.pl>
2017-04-01 16:47:55 +0200

    Toggle the menu item

--- app/src/main/java/pl/korlotian/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/pl/korlotian/photogallery/PhotoGalleryFragment.java
@@ -118,6 +118,13 @@ public class PhotoGalleryFragment extends Fragment {
                 searchView.setQuery(query, false);
             }
         });
+
+        MenuItem toggleItem = menu.findItem(R.id.menu_item_toggle_polling);
+        if (PollService.isServiceAlarmOn(getActivity())) {
+            toggleItem.setTitle(R.string.stop_polling);
+        } else {
+            toggleItem.setTitle(R.string.start_polling);
+        }
     }
 
     @Override

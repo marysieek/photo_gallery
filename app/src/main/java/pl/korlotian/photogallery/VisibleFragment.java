3311cf8310fe63d1a59b65fa63740fe9cd48184a
Maria Korlotian <maria.stoklosa@netguru.pl>
2017-04-02 17:52:46 +0200

    Send result back

--- app/src/main/java/pl/korlotian/photogallery/VisibleFragment.java
+++ app/src/main/java/pl/korlotian/photogallery/VisibleFragment.java
@@ -1,10 +1,12 @@
 package pl.korlotian.photogallery;
 
+import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.widget.Toast;
 
 public abstract class VisibleFragment extends Fragment {
@@ -27,10 +29,8 @@ public abstract class VisibleFragment extends Fragment {
     private BroadcastReceiver mOnShowNotification = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
-            Toast.makeText(getActivity(),
-                    "Got a broadcast:" + intent.getAction(),
-                    Toast.LENGTH_LONG)
-                    .show();
+            Log.i(TAG, "cancelling notification");
+            setResultCode(Activity.RESULT_CANCELED);
         }
     };
 }
